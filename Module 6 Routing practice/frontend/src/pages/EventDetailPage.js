d563c9602cd219f224bd73a46f5db9c47461e689
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2024-11-01 16:11:24 -0500

    Update EventDetailPage.js

--- Module 6 Routing practice/frontend/src/pages/EventDetailPage.js
+++ Module 6 Routing practice/frontend/src/pages/EventDetailPage.js
@@ -1,15 +1,38 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useRouteLoaderData("event-detail");
 
-  return (
-    <>
-      <h1>Event Detail Page!</h1>
-      <p>Event ID : {params.eventId}</p>
-    </>
-  );
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Couldnt fetch details for selected event." },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
+}
+
+export async function action({ request, params }) {
+  const eventId = params.eventId;
+  const response = await fetch(`http://localhost:8080/events/${eventId}`, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Couldn't delete event" }, { status: 500 });
+  }
+  return redirect("/events");
+}
