8cedc2372916ebb4c11e2b425605a30f9b286af1
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2024-10-31 23:03:58 -0500

    Update EventPage.js

--- Module 6 Routing practice/frontend/src/pages/EventPage.js
+++ Module 6 Routing practice/frontend/src/pages/EventPage.js
@@ -1,24 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Lorem ipsum" },
-  { id: "e2", title: "Lorem Ipsum random" },
-];
+import EventsList from "../components/EventsList";
 
-function EventPage() {
-  return (
-    <>
-      <h1>Event Page !</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={`/events/${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+function EventsPage() {
+  const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
+  const events = data.events;
+  return <EventsList events={events} />;
 }
 
-export default EventPage;
+export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
+    // return json({ message: "Could not fetch events" }, { status: 500 });
+  } else {
+    return response;
+  }
+}
