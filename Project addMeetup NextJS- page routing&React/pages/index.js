15e5a994649e1930316232597c32ef6878db1704
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2024-11-25 10:24:39 -0600

    Add files via upload

---
+++ Project addMeetup NextJS- page routing&React/pages/index.js
@@ -0,0 +1,54 @@
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+function HomePage(props) {
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge of highly active React meetups."
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   //fetch data
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: { meetups: DUMMY_MEETUPS },
+//   };
+// }
+
+export async function getStaticProps() {
+  //fetch data from API
+  const client = await MongoClient.connect("YOUR_MONGODB_LINK");
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
