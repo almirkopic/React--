8599595c7442420c01d5b8cbeb9aca38ba570db7
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2024-12-05 21:38:12 -0600

    Update Modal.jsx

--- React practice App-Router&Data Fetch&loader&action/src/components/Modal.jsx
+++ React practice App-Router&Data Fetch&loader&action/src/components/Modal.jsx
@@ -4,14 +4,19 @@ import { useNavigate } from "react-router-dom";
 
 function Modal({ children }) {
   const navigate = useNavigate();
-  function closeHandler() {
+
+  function onClose() {
     navigate("/");
+  }
+
+  function stopPropagation(event) {
+    event.stopPropagation();
   }
 
   return (
     <>
-      <div className={classes.backdrop}>
-        <dialog open={true} className={classes.modal}>
+      <div className={classes.backdrop} onClick={onClose}>
+        <dialog open={true} className={classes.modal} onClick={stopPropagation}>
           {children}
         </dialog>
       </div>
